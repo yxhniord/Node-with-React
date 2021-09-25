02ff2b6900fa5938228bd0e3de847b5babf6027b
yxhniord <yxhniord@gmail.com>
2021-09-25 15:15:15 -0700

    finished session 3

--- server/index.js
+++ server/index.js
@@ -1,10 +1,38 @@
 // node仅支持这种CommonJS的规范，不支持es2015的import方式导入包
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
-});
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        },
+        (accessToken, refreshToken, profile, done) => {
+            console.log('accessToken', accessToken);
+            console.log('refreshToken', refreshToken);
+            console.log('profile', profile);
+            console.log('done', done);
+        }
+    )
+);
+
+app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
+
+app.get(
+    '/auth/google/callback',
+    passport.authenticate('google')
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
